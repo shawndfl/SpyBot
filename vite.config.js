39e5793343b54fb200ea057bb1f624a8395df081
shawn <none@localhost.com>
2026-03-13 00:40:10 -0400

    Add some unit tests

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   build: {
     minify: false, // Disable minification of the final output
   },
-};
+  test: {
+    globals: true,
+    environment: 'node',
+    typecheck: {
+      tsconfig: './tsconfig.test.json',
+    },
+    include: ['tests/**/*.test.ts'],
+  },
+});
